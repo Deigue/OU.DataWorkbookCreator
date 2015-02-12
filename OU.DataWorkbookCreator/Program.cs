69a5deb4ebb61a79b31be6add60f0b4ee17cbb6c
Deigue <deigue@hotmail.com>
2015-02-12 01:30:09 -0500

    Basic project setup done.

--- OU.DataWorkbookCreator/Program.cs
+++ OU.DataWorkbookCreator/Program.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace OU.DataWorkbookCreator
 {
-    class Program
+    public class Program
     {
+        private string CreateTemplateCopy(string[] output)
+        {
+            string outputDirectory = output[0];
+            string outputFileName = output[1];
+            string inputPath = @"../../Template/Template.xlsx";
+            if (!Directory.Exists(outputDirectory))
+                Directory.CreateDirectory(outputDirectory);
+            string outputFullPath = Path.Combine(outputDirectory, outputFileName);
+            if(File.Exists(inputPath))
+                System.IO.File.Copy(inputPath, outputFullPath, true);
+            return outputFullPath;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("_______________Workbook Creator_______________\n");
+
+            var workbookCreator = new Program();
+
         }
     }
 }
