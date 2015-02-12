b9fce806d5ec302423390877fb68308079030dd2
Deigue <deigue@hotmail.com>
2015-02-12 01:01:35 -0500

    Solution for Template Workbook Creator created.

---
+++ OU.DataWorkbookCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace OU.DataWorkbookCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
